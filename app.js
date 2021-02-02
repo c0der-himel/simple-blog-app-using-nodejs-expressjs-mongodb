b8006a24735b063f001bc2bdf9a325f1e2de8b92
himel <himel777hussain@gmail.com>
2021-02-02 16:40:37 +0600

    database and operations added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 const app = express();
 
@@ -8,10 +10,60 @@ app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 // Home Page
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+  res.redirect('/blogs');
+});
+
+// All Blogs
+app.get('/blogs', (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('blogs', { title: 'All Blogs', blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findById(id)
+    .then((result) => {
+      res.render('details', { title: 'Blog Details', blog: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // About Page
@@ -29,8 +81,20 @@ app.use((req, res) => {
   res.status(404).render('404', { title: '404 Page' });
 });
 
+const username = 'dev-blog-0-admin';
+const password = 'admin123';
+const dbName = 'dev-blog-0';
 const PORT = process.env.PORT || 3000;
+const dbURI = `mongodb+srv://${username}:${password}@nodejs.2gw6h.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
-});
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(PORT, () => {
+      console.log('Connected to Database');
+      console.log(`Server is running on PORT: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
