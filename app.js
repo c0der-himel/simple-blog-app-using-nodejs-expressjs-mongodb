257a12ff4591ada6c616e1e24f421b64011b4cf5
himel <himel777hussain@gmail.com>
2021-02-02 12:20:12 +0600

    view engine setup done

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const express = require('express');
 
 const app = express();
 
+// View Engine
+app.set('view engine', 'ejs');
+
+// Home Page
 app.get('/', (req, res) => {
-  res.send(/* html */ `<h1>Home Page</h1>`);
+  res.render('index', { title: 'Home' });
+});
+
+// About Page
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// Create Blog
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create Blog' });
+});
+
+// 404 Page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404 Page' });
 });
 
 const PORT = process.env.PORT || 3000;
